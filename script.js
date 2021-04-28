6d95a4476a506b906049a17fed05fff60f006375
williamf-r <32742939+williamfelixr@users.noreply.github.com>
2021-04-28 19:33:08 +0100

    add automatic image scroll and update counter

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
 var imgArray = ["media/bridge.jpg", "media/dog.jpg", "media/grass.jpg", "media/lake.jpg", "media/lichen.jpg", "media/mill.jpg"];
-      var x = 1;
-      var image = document.getElementById("slide");
-      
-      function nextImage() {
-        if (x >= imgArray.length - 1) {
-          x = 0;
-        } else {
-          x++
-        } /* x increases by 1 each time function is called, returning to zero after final index in imgArray */
-        
-        image.src = imgArray[x]
-      }
+var x = 0;
+var image = document.getElementById("slide");
+var imageCounter = document.getElementById("image-counter");
+imageCounter.innerText = (x + 1) + "/6"
+    
+document.addEventListener("keydown", function(event){
+  if (event.key == "ArrowLeft") {
+    previousImage();
+  } else if (event.key =="ArrowRight") {
+    nextImage();
+  }
 
-      function previousImage() {
-        if (x <= 0){
-          x = imgArray.length - 1;
-        } else {
-          x--
-        } // x decreases by 1 each time function is called, returning to highest index after reaching zero
+})
 
-        image.src = imgArray[x]
-      }
 
-    document.addEventListener("keydown", function(event){
-      if (event.key == "ArrowLeft") {
-        previousImage();
-      } else if (event.key =="ArrowRight") {
-        nextImage();
-      }
 
-    })
+function nextImage() {
+  if (x >= imgArray.length - 1) {
+    x = 0;
+  } else {
+    x++
+  } /* x increases by 1 each time function is called, returning to zero after final index in imgArray */
+
+  image.src = imgArray[x];
+  console.log(image.src);
+  imageCounter.innerText = (x + 1) + "/6"
+}
+
+function previousImage() {
+  if (x <= 0){
+    x = imgArray.length - 1;
+  } else {
+    x--
+  } // x decreases by 1 each time function is called, returning to highest index after reaching zero
+
+  image.src = imgArray[x];
+  console.log(image.src);
+  imageCounter.innerText = (x + 1) + "/6"
+}
+
+setInterval(nextImage, 4000);
+
+
 
